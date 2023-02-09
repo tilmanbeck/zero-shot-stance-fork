22cdffebe612e3c1fd1a7f1415c1dbcf3bebaa09
Tilman Beck <beck@ukp.informatik.tu-darmstadt.de>
2023-02-09 17:29:19 +0100

    minor stuff

--- src/clustering/stance_clustering.py
+++ src/clustering/stance_clustering.py
@@ -143,18 +143,18 @@ def save_bert_vectors(datasetpath, embed_model, dataloader, batching_fn, batchin
     # np.save('../../resources/topicreps/bert_topic-{}.vecs.npy'.format(dataname), topicm)
     np.save(datasetpath + '/bert_topic-{}.vecs.npy'.format(dataname), topicm)
     del topicm
-    print("[{}] saved to ../../resources/topicreps/bert_[tfidfW_doc/topic]-{}.vecs.npy".format(dataname, dataname))
+    print("[{}] saved to {}/bert_[tfidfW_doc/topic]-{}.vecs.npy".format(dataname, datasetpath, dataname))
 
     pickle.dump(doc2i, open(datasetpath + '/bert_tfidfW_doc-{}.vocab.pkl'.format(dataname), 'wb'))
     pickle.dump(topic2i, open(datasetpath + '/bert_topic-{}.vocab.pkl'.format(dataname), 'wb'))
-    print("[{}] saved to ../../resources/topicreps/bert_[tfidfW_doc/topic]-{}.vocab.pkl".format(dataname, dataname))
+    print("[{}] saved to datasetpath/resources/topicreps/bert_[tfidfW_doc/topic]-{}.vocab.pkl".format(dataname, datasetpath, dataname))
 
 
 def load_vector_data(p, docname, topicname, dataname, dataloader, mode='concat'):
-    docm = np.load('{}{}-{}.vecs.npy'.format(p, docname, dataname))
-    topicm = np.load('{}{}-{}.vecs.npy'.format(p, topicname, dataname))
-    doc2i = pickle.load(open('{}{}-{}.vocab.pkl'.format(p, docname, dataname), 'rb'))
-    topic2i = pickle.load(open('{}{}-{}.vocab.pkl'.format(p, topicname, dataname), 'rb'))
+    docm = np.load('{}/{}-{}.vecs.npy'.format(p, docname, dataname))
+    topicm = np.load('{}/{}-{}.vecs.npy'.format(p, topicname, dataname))
+    doc2i = pickle.load(open('{}/{}-{}.vocab.pkl'.format(p, docname, dataname), 'rb'))
+    topic2i = pickle.load(open('{}/{}-{}.vocab.pkl'.format(p, topicname, dataname), 'rb'))
 
     doc2topics = dict()
     unique_topics = set()
